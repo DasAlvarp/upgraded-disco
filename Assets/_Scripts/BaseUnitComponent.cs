a903ada7ac27d786e0b6a3b18fe4dc4ecd879627
alvarpq <alvarogudis@gmail.com>
2016-06-13 14:17:52 -0400

    initial commit

---
+++ Assets/_Scripts/BaseUnitComponent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseUnitComponent : MonoBehaviour
+{
+
+    public int health;
+    public int speed;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/_Scripts/CharacterCombatController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterCombatController : MonoBehaviour
+{
+    public int speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        transform.GetComponent<Rigidbody>().velocity = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical")) * speed;
+    }
+}
